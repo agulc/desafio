fddd79a2eed29027415292f42e0868892cc6db6a
Agustin Cao <alcaolpg@gmail.com>
2023-06-02 22:16:30 -0300

    add project

---
+++ src/main/java/com/agulc/apidanaide/ApidanaideApplication.java
@@ -0,0 +1,13 @@
+package com.agulc.apidanaide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidanaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidanaideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agulc/apidanaide/ApidanaideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agulc.apidanaide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidanaideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
