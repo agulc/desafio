b9f227bd6e01c778c8d2271a746570b2bd8882eb
Agustin Cao <alcaolpg@gmail.com>
2023-06-03 03:27:26 -0300

    add Unimplemented carrito service

---
+++ src/main/java/com/agulc/apidanaide/services/implementations/CarritoServiceImpl.java
@@ -0,0 +1,52 @@
+package com.agulc.apidanaide.services.implementations;
+
+import java.util.List;
+
+import com.agulc.apidanaide.entities.Carrito;
+import com.agulc.apidanaide.services.CarritoService;
+
+public class CarritoServiceImpl implements CarritoService {
+
+    @Override
+    public Long crearCarrito(Long dniUsuario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crearCarrito'");
+    }
+
+    @Override
+    public Carrito getCarritoById(Long idCarrito) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCarritoById'");
+    }
+
+    @Override
+    public Carrito addProductoToCarrito(Long idCarrito, Long idProducto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addProductoToCarrito'");
+    }
+
+    @Override
+    public Carrito removeProductoFromCarrito(Long idCarrito, Long idProducto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeProductoFromCarrito'");
+    }
+
+    @Override
+    public List<Carrito> getAllCarritos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCarritos'");
+    }
+
+    @Override
+    public Carrito updateCarrito(Carrito carrito) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCarrito'");
+    }
+
+    @Override
+    public void deleteCarrito(Long idCarrito) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCarrito'");
+    }
+    
+}
